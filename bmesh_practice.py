9eb79125d9274bdf61ae332318f8d671dafdec48
teruma0 <55909355+watanarishi@users.noreply.github.com>
2021-02-12 22:29:16 +0900

    大幅に変更

--- bmesh_practice.py
+++ bmesh_practice.py
@@ -1,22 +1,21 @@
 # -*- encoding:utf-8 -*-
 
 import bpy
-import math
-import random
-import os
 import bmesh
+import random
 
-def all_activate():
-    for all in bpy.data.objects:
-        bpy.context.view_layer.objects.active = all
 
 def all_select():
     for all in bpy.data.objects:
         all.select_set(True)
 
-def all_deselect():
+def deselect_all():
     for all in bpy.data.objects:
         all.select_set(False)
+
+def all_activate():
+    for all in bpy.data.objects:
+        bpy.context.view_layer.objects.active = all
 
 def all_delete():
     for i in bpy.data.objects:
@@ -28,29 +27,45 @@ def all_delete():
 
 all_delete()
 
-obj_path = bpy.path.abspath('/Users/wakabayashiteruma/Desktop/room.obj')
-bpy.ops.import_scene.obj(filepath = obj_path)
-ceiling = bpy.data.objects["textured_output.001"]
-ceiling.hide_viewport = True
+def add_solid(num,x,y):
+    bpy.ops.mesh.primitive_plane_add(location=(0,0,0))
+    active = bpy.context.object
+    active.scale=(x*1.5,y*1.5,0.1)
+    for sld in range(num):
+        rand_x = random.uniform(1,x/2)
+        rand_y = random.uniform(1,y/2)
+        bpy.ops.mesh.primitive_cube_add(location=(0,0,0))
+        active = bpy.context.object
+        active.location.x = random.uniform(-x,x)
+        active.location.y = random.uniform(-y,y)
+        active.location.z += rand_x
+        active.scale=(rand_x,rand_y,rand_x)
+    return
+num_cube = 5
 
-active = bpy.context.object.data
+add_solid(num_cube,10,10)
+all_select()
+bpy.ops.object.join()
+
+active1 = bpy.context.object.data
 bm = bmesh.new()
-bm.from_mesh(active)
+bm.from_mesh(active1)
 """
 for obj in bpy.data.objects:
         if obj.name.startswith('Cube'):
             bpy.context.view_layer.objects.active = obj
 """
-for v in range():
+#active = bpy.data.objects['Cube.003']
+
+
+for i in range(num_cube*8):
     list(bm.verts)[i].select_set(True)
     x = list(bm.verts)[i].co.x
     y = list(bm.verts)[i].co.y
     z = list(bm.verts)[i].co.z
-    if  z >= 1.5:
-        list(bm.verts)[i].co.z +=5
-        bm.to_mesh(active)
-    else:
-        bm.to_mesh(active)
-    return
+    if  z >= 1:
+        list(bm.verts)[i].co.z += random.uniform(1,10)
+    bm.to_mesh(active1)
 
+deselect_all()
 bm.free()
